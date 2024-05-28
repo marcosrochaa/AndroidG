024387f35375df0a6ea6b26b2feed235e7bcfedf
marocha <marcosrocha0096@gmaail.com>
2024-05-28 14:43:53 -0300

    Merge remote-tracking branch 'origin/master'

--- app/src/main/java/com/globo/aprendizado/MainActivity.kt
+++ app/src/main/java/com/globo/aprendizado/MainActivity.kt
@@ -1,19 +1,16 @@
 package com.globo.aprendizado
 
-import android.annotation.SuppressLint
-import android.os.AsyncTask
 import android.os.Bundle
 import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
 import com.globo.aprendizado.adapter.CarroAdapter
 import com.globo.aprendizado.adapter.CarroCallback
 import com.globo.aprendizado.model.Carro
-import org.json.JSONArray
-import java.net.HttpURLConnection
-import java.net.URL
+import com.globo.aprendizado.model.Retrofit.carService
+import retrofit2.Call
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -28,64 +25,24 @@ class MainActivity : AppCompatActivity() {
 
         })
         listView.adapter = adapter
-        WebAsyncTask(lista, adapter).execute()
 
-    }
+        val call = carService.getCarros()
 
-
-    class WebAsyncTask(
-        private val listaCarros: MutableList<Carro>,
-        private val adapter: CarroAdapter
-    ) : AsyncTask<String, Void, Boolean>() {
-        override fun doInBackground(vararg params: String?): Boolean {
-            return try {
-
-
-                val url = URL("https://9jg03.wiremockapi.cloud/carros")
-
-                val connection = url.openConnection() as HttpURLConnection
-                connection.requestMethod = "GET"
-                connection.connectTimeout = 10000
-                connection.readTimeout = 10000
-
-                val responseCode = connection.responseCode
-
-                println(responseCode)
-                if (responseCode == 200) {
-                    //sucesso
-                    val inputStream = connection.inputStream
-                    val response = inputStream.bufferedReader().use { it.readText() }
-
-                    val jsonArray = JSONArray(response)
-
-                    for (i in 0 until jsonArray.length()) {
-                        val json = jsonArray.getJSONObject(i)
-
-                        val carro = Carro(
-                            json.getString("modelo"),
-                            json.getString("imagem"),
-                            json.getString("ano"),
-                            json.getString("cor"),
-                            json.getDouble("preco"),
-                            json.getString("marca"),
-                        )
-
-                        listaCarros.add(carro)
-                    }
-
-                    println(listaCarros)
-
+        call.enqueue(object : retrofit2.Callback<List<Carro>> {
+            override fun onResponse(p0: Call<List<Carro>>, p1: Response<List<Carro>>) {
+                if (p1.code() == 200) {
+                    lista.addAll(p1.body().orEmpty())
+                    adapter.notifyDataSetChanged()
+                } else {
+                    println("Erro!")
                 }
-
-                return true
-            } catch (e : Exception){
-                print(e.toString())
-                return false
             }
-        }
 
-        override fun onPostExecute(result: Boolean?) {
-            adapter.notifyDataSetChanged()
-        }
+            override fun onFailure(p0: Call<List<Carro>>, p1: Throwable) {
+                println("Erro!")
+            }
+
+        })
+
     }
 }
